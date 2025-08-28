cc9c5e9dcba540661ef7b1e1e4014e7bd949d6d9
Sanjay-geospatial <sanjaymanjappa25@gmail.com>
2025-08-28 19:12:03 +0530

    Update data.py

--- data.py
+++ data.py
@@ -81,5 +81,9 @@ def combine_data(s2, s1, dem, month):
   x2 = s1.sel(month = month).to_array(dim = 'band').drop_vars(['month'], errors='ignore')
   x3 = dem.to_array().squeeze().expand_dims(dim  = {'band' : ['dem']}).drop_vars(['time', 'variable'], errors='ignore')
 
+ st.write(f"s2 coords: {x1.coords}")
+ st.write(f"s1 coords: {x2.coords}")
+ st.write(f"dem coords: {x3.coords}")
+
   combined = xr.concat([x1, x2, x3], dim = 'band')
   return combined
