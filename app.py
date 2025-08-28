f3b40fcc310c8aa640c5862124c5eed5c4469e99
Sanjay-geospatial <Administrator@L-INBA-Aariya01.ap.etcred.com>
2025-08-28 14:24:19 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,108 @@
+import joblib
+import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import matplotlib.colors as mcolors
+import xarray as xr
+import rioxarray as rxr
+import pandas as pd
+import numpy as np
+import os
+import random
+import geopandas as gpd
+import pystac_client
+import odc.stac
+import data
+
+st.set_page_config(
+    page_title="Land cover app",
+    page_icon="☘🌳🌴",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+model = joblib.load('lulc_model.pkl')
+
+st.title("☁️→🌤️ Land cover App")
+st.write("Prototype for Land cover analysis")
+
+st.sidebar.title("Select Date")
+year = st.sidebar.selectbox("Year", [2020, 2021, 2022, 2023])
+month = st.sidebar.selectbox("Month", list(range(1, 6)))
+
+s2_monthly, s1_monthly, dem = data.get_satellite_data(shapefile_path = '/data/Chapuralapalli.shp',
+                                  start_date = f'{year}-{month}-01',
+                                  end_date = f'{year}-{month+3}-01')
+
+combined_data = data.combine_data(s2_monthly, s1_monthly, dem, month)
+
+gdf = gpd.read_file('data/Chapuralapalli.shp')
+
+raster_df = pd.DataFrame()
+
+for i in combined_data.band.values:
+  raster_df[i] = combined_data.sel(band = i).values.flatten()
+  raster_df[i].fillna(raster_df[i].mean(), inplace = True)
+
+predicted = model.predict(raster_df)
+predicted_reshaped = predicted.reshape(combined_data.shape[1], combined_data.shape[2])
+
+predicted_array = xr.DataArray(
+    data=predicted_reshaped,
+    coords={
+        "y": combined_data.y,
+        "x": combined_data.x
+    },
+    dims=["y", "x"],
+    name="lulc"
+)
+
+predicted_array = predicted_array.rio.write_crs(combined_data.rio.crs)
+
+gdf = gdf.to_crs(predicted_reshaped.rio.crs)
+
+predicted_reshaped_clip = predicted_array.rio.clip(
+    gdf.geometry,
+    crs=gdf.crs,
+    drop=False, 
+    all_touched=True 
+)
+
+classes = {
+    0: "Barren land",
+    1: "Water",
+    2: "Agricultural land",
+    3: "Settlements",
+    4: "Forest",
+}
+colors = ["saddlebrown", "blue", "red", "magenta", "green"]
+
+cmap = mcolors.ListedColormap(colors)
+bounds = np.arange(-0.5, len(classes)+0.5, 1)
+norm = mcolors.BoundaryNorm(bounds, cmap.N)
+
+# Create figure
+fig, ax = plt.subplots(figsize=(8,6))
+im = ax.imshow(predicted_reshaped_clip, cmap=cmap, norm=norm)
+ax.axis("off")
+ax.set_title(f"Predicted LULC for {month}/{year}")
+
+# Create legend
+legend_elements = [Patch(facecolor=colors[i], label=classes[i]) for i in classes]
+ax.legend(handles=legend_elements, bbox_to_anchor=(1.05, 1), loc='upper left')
+
+# Display in Streamlit
+st.pyplot(fig)
+
+pixel_area_m2 = 10*10  # example if 10m resolution
+unique, counts = np.unique(predicted_reshaped_clip[~np.isnan(predicted_reshaped_clip)], return_counts=True)
+area_sqkm = counts * pixel_area_m2 / 1e6
+area_dict = {classes[int(k)]: v for k,v in zip(unique, area_sqkm)}
+
+# --- Bar Plot ---
+fig2, ax2 = plt.subplots(figsize=(8,4))
+ax2.bar(area_dict.keys(), area_dict.values(), color=[colors[int(k)] for k in unique])
+ax2.set_ylabel("Area (sq. km)")
+ax2.set_title("Area of Each LULC Class")
+ax2.set_xticklabels(area_dict.keys(), rotation=45, ha='right')
+
+st.pyplot(fig2)
---
+++ data.py
@@ -0,0 +1,77 @@
+def get_satellite_data(shapefile_path, start_date, end_date):
+  gdf = gpd.read_file(shapefile_path)
+  bounds = gdf.total_bounds
+  bbox = (float(bounds[0]), float(bounds[1]), float(bounds[2]), float(bounds[3]))
+
+  STAC_URL = "https://earth-search.aws.element84.com/v1"
+
+  catalog = pystac_client.Client.open(STAC_URL)
+
+  search = catalog.search(
+      collections = 'sentinel-2-l2a',
+      datetime = start_date+'/'+end_date,
+      bbox = bbox,
+      query = {'eo:cloud_cover': {'lt': 10}}
+  )
+
+  items = list(search.get_all_items())
+  print(len(items))
+
+  bands = ['red', 'green', 'blue', 'nir', 'nir08', 'nir09', 'rededge1', 'rededge2', 'rededge3', 'swir16', 'swir22']
+
+  ds = odc.stac.load(
+      items = items,
+      bands = bands,
+      bbox= bounds,
+      crs= 32643,
+      resolution=10
+  )
+
+  monthly_data = ds.groupby('time.month').median(dim = 'time')
+  monthly_data = monthly_data.to_array(dim = 'band')
+
+  s1_search = catalog.search(
+    collections = 'sentinel-1-grd',
+    datetime = '2023-01-01/2023-04-01',
+    bbox = bbox
+)
+
+  s1_items = list(s1_search.get_all_items())
+
+  s1_da = odc.stac.load(
+      items = s1_items,
+      bands = ['vv', 'vh'],
+      bbox = bbox,
+      crs = 32643,
+      resolution = 10
+  )
+
+  s1_da_monthly = s1_da.groupby('time.month').median(dim = 'time')
+
+  dem_search = catalog.search(
+    collections=["cop-dem-glo-30"],
+    bbox=bbox
+)
+
+  # Get items from DEM search
+  dem_items = list(dem_search.get_all_items())
+
+  # Load DEM with odc.stac
+  dem_da = odc.stac.load(
+      items=dem_items,
+      bbox=bbox,
+      crs="EPSG:32643",   # UTM zone 43N
+      resolution=10       # resample DEM to 10 m (original is 30 m)
+  )
+  dem_da
+
+  return monthly_data, s1_da_monthly, dem_da
+
+def combine_data(s2, s1, dem, month):
+  x1 = s2.sel(month = month).drop_vars(['month'], errors='ignore')
+  x2 = s1.sel(month = month).to_array(dim = 'band').drop_vars(['month'], errors='ignore')
+  x3 = dem.to_array().squeeze().expand_dims(dim  = {'band' : ['dem']}).drop_vars(['time', 'variable'], errors='ignore')
+
+  combined = xr.concat([x1, x2, x3], dim = 'band')
+  return combined
+
